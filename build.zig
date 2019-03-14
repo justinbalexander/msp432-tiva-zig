c6a9bd1ede48ad9b0dc70b357925f8d55a1ad5e7
vegecode <justin.b.alexander1@gmail.com>
2019-03-13 23:16:00 -0500

    Fill out build script to build for armv7em

--- build.zig
+++ build.zig
@@ -1,14 +1,22 @@
 const Builder = @import("std").build.Builder;
+const builtin = @import("builtin");
+
+const root_src = "src/startup.zig";
+const out_file_name = "msp432-tiva-zig";
+const out_dir = "zig-cache";
+const linker_script = "link/project.ld";
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
-    const exe = b.addExecutable("msp432-tiva-zig", "src/main.zig");
+
+    const arch = builtin.Arch{ .arm = builtin.Arch.Arm32.v7em };
+    const environ = builtin.Abi.eabihf;
+
+    const exe = b.addExecutable(out_file_name, root_src);
+    exe.setOutputDir(out_dir);
     exe.setBuildMode(mode);
-
-    const run_cmd = exe.run();
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    exe.setTarget(arch, builtin.Os.freestanding, environ);
+    exe.setLinkerScriptPath(linker_script);
 
     b.default_step.dependOn(&exe.step);
     b.installArtifact(exe);
